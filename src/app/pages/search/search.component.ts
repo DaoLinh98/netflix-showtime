d1a5709d18d12251718318fd8ed92e6c634d2213
ĐaoLinh89 <94506881+DaoLinh98@users.noreply.github.com>
2023-06-18 16:31:40 +0700

    init project

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MovieApiServiceService } from 'src/app/services/movie-api-service.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  constructor(private service: MovieApiServiceService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  searchResult: any;
+  searchForm = new FormGroup({
+    'movieName': new FormControl(null)
+  });
+
+  submitForm() {
+    console.log(this.searchForm.value, 'searchform#');
+    this.service.getSearchMovie(this.searchForm.value).subscribe((result) => {
+      console.log(result, 'searchmovie##');
+      this.searchResult = result.results;
+    });
+  }
+
+}
